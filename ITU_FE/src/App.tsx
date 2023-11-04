1eecf3fb71a35c0653b9659a491d17c18d41413d
Andrej Mokris <mokris.andrej@gmail.com>
2023-11-04 22:46:02 +0100

    Initialize React app with tailwind

---
+++ ITU_FE/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex w-full h-screen items-center justify-center">
+        <p className="font-bold text-4xl underline">Hello from ITU APP</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
